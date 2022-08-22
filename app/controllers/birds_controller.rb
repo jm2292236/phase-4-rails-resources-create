f292084da00fa73ca66582f3453abe0e09b79666
Jaime Montes de Oca <jaime0422@live.com>
2022-08-22 13:35:33 -0700

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,19 +1,32 @@
 class BirdsController < ApplicationController
 
-  # GET /birds
-  def index
-    birds = Bird.all
-    render json: birds
-  end
-
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+    # GET /birds
+    def index
+        birds = Bird.all
+        render json: birds
     end
-  end
+
+    # GET /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+        render json: bird
+        else
+        render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
+
+    # POST /birds
+    def create
+        byebug
+        new_bird = Bird.create(name: params[:name], species: params[:species])
+        render json: new_bird, status: :created
+    end
+
+    # DELETE /birds/:id
+    def destroy
+        Bird.destroy(params[:id])
+        render json: {message: "Bird deleted"}
+    end 
 
 end
